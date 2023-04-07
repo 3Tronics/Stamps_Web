931248951a85804646bbbbf1fbec167a42fc2f8b
3Tronics <rol3@gmx.com>
2023-04-07 12:52:21 -0700

    V01.52_04

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
   <link rel="stylesheet" href="/src/modal.css">
   <link rel="stylesheet" href="/src/style.min_formatted.css" />
 
-   <title> V01.52_02</title>
+   <title> V01.52_04</title>
 
 
 <style>
