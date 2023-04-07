1bec8b1b520f74900384d16c56a2c72fc6e1f93c
3Tronics <rol3@gmx.com>
2023-04-07 12:26:46 -0700

    V01.52_02

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
   <link rel="stylesheet" href="/src/modal.css">
   <link rel="stylesheet" href="/src/style.min_formatted.css" />
 
-   <title> This is the Home page V01.52_01</title>
+   <title> V01.52_02</title>
 
 
 <style>
