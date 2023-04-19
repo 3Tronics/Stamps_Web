0bad55da4156d65bd6208a6bbd3808630b74413a
3Tronics <rol3@gmx.com>
2023-04-19 09:53:49 -0700

    test echo12

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
   <link rel="stylesheet" href="/src/modal.css">
   <link rel="stylesheet" href="/src/style.min_formatted.css" />
 
-  <title> V01.52_11</title>
+  <title> V01.52_12</title>
 
 <style>
 		/*
