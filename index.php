059f40515ff5a8b55a5e0cd530af913852c021da
3Tronics <rol3@gmx.com>
2023-04-19 10:18:39 -0700

    test22

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
   <link rel="stylesheet" href="/src/modal.css">
   <link rel="stylesheet" href="/src/style.min_formatted.css" />
 
-  <title> V01.52_21</title>
+  <title> V01.52_22</title>
 
 <style>
 		/*
